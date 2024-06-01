df7c213454ed7bfa3e5111c624fd147a261723f7
Sean <seanyiap@gmail.com>
2024-06-01 14:49:36 +0100

    grid formatting

--- src/components/CountryDataGrid.tsx
+++ src/components/CountryDataGrid.tsx
@@ -1,48 +1,31 @@
 import { FC, useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
-import { ColDef } from "ag-grid-community";
+import { ColDef, GridOptions } from "ag-grid-community";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
+import { CountriesData } from "../types";
 
 const API_ENDPOINT = "https://restcountries.com/v3.1/all";
 
 interface CountryDataGridProps {}
 
+const countryCellRenderer = ({ data }) => {
+  return (
+    <span>
+      <img
+        width="20px"
+        alt={`${data.name.common} flag`}
+        style={{ marginRight: 8 }}
+        src={`https://flagcdn.com/h20/${data.cca2.toLowerCase()}.png`}
+      />
+      {data.name.common}
+    </span>
+  );
+};
+
 const CountryDataGrid: FC<CountryDataGridProps> = ({}) => {
   const [rowData, setRowData] = useState([]);
   const [error, setError] = useState(null);
-
-  const columnDefs: ColDef[] = [
-    {
-      headerName: "Name",
-      field: "name.common",
-      sort: "asc",
-    },
-    {
-      headerName: "Capital",
-      field: "capital",
-    },
-    {
-      headerName: "Languages",
-      field: "languages",
-      valueGetter: (p) =>
-        p.data.languages ? Object.values(p.data.languages) : "-",
-    },
-    {
-      headerName: "Population",
-      field: "population",
-      valueFormatter: (p) => {
-        let num = parseFloat(p.data.population) ?? 0;
-        return num.toLocaleString("en-US");
-      },
-    },
-    {
-      headerName: "Currencies",
-      field: "currencies",
-      valueGetter: (p) =>
-        p.data.currencies ? Object.keys(p.data.currencies) : "-",
-    },
-  ];
 
   useEffect(() => {
     const fetchGridData = async () => {
@@ -60,6 +43,54 @@ const CountryDataGrid: FC<CountryDataGridProps> = ({}) => {
     fetchGridData();
   }, []);
 
+  const columnDefs: ColDef[] = [
+    {
+      headerName: "Name",
+      field: "name.common",
+      sort: "asc",
+      flex: 2,
+      cellRenderer: countryCellRenderer,
+    },
+    {
+      headerName: "Capital",
+      field: "capital",
+    },
+    {
+      headerName: "Languages",
+      field: "languages",
+      valueGetter: (p) =>
+        p.data.languages ? Object.values(p.data.languages) : "-",
+      flex: 2,
+    },
+    {
+      headerName: "Population",
+      field: "population",
+      valueFormatter: (p) => {
+        let num = parseFloat(p.data.population) ?? 0;
+        return num.toLocaleString("en-US");
+      },
+    },
+    {
+      headerName: "Currencies",
+      field: "currencies",
+      valueGetter: (p) =>
+        p.data.currencies ? Object.keys(p.data.currencies) : "-",
+    },
+  ];
+
+  const gridOptions: GridOptions<CountriesData> = {
+    defaultColDef: {
+      flex: 1,
+      editable: false,
+      sortable: true,
+      minWidth: 100,
+      filter: true,
+    },
+    rowData: [],
+    columnDefs: columnDefs,
+    animateRows: true,
+  };
+
   return (
     <div
       className="ag-theme-alpine"
@@ -69,7 +100,8 @@ const CountryDataGrid: FC<CountryDataGridProps> = ({}) => {
       }}
     >
       <AgGridReact
-        defaultColDef={{ sortable: true, filter: true }}
+        gridOptions={gridOptions}
+        rowSelection={"single"}
         rowData={rowData}
         columnDefs={columnDefs}
         pagination={true}
---
+++ src/types.ts
@@ -0,0 +1,40 @@
+export interface CountriesData {
+  name: {
+    common: string;
+    official: string;
+    nativeName: { [abbr: string]: { official: string; common: string } };
+  };
+  tld: string[];
+  cca2: string;
+  cca3: string;
+  ccn3: string;
+  cioc: string;
+  independent: boolean;
+  status: string;
+  unMember: boolean;
+  currencies?: { [currency: string]: { name: string; symbol: string } };
+  idd: { root: string; suffixes: string[] };
+  capital: string[];
+  altSpellings: string[];
+  region: string;
+  subregion: string;
+  languages?: { [language: string]: string };
+  translations: { [language: string]: { official: string; common: string } };
+  langlng: number[];
+  landlock: boolean;
+  borders: string[];
+  area: number;
+  demonyms: { [demonym: string]: { f: string; m: string } };
+  flag: string;
+  maps: { googleMaps: string; openStreetMaps: string };
+  population: number;
+  gini: { [year: string]: number };
+  fifa: string;
+  timezones: string[];
+  continents: string[];
+  flags: { alt: string; png: string; svg: string };
+  coatOfArms: { png: string; svg: string };
+  startOfWeek: string;
+  capitalInfo: { [latlng: string]: number[] };
+  postalCode: { format: string; regex: string };
+}
