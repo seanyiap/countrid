1d9fb9b17e78294c4948ea2a881d382673905cdd
Sean <seanyiap@gmail.com>
2024-06-01 17:19:17 +0100

    added mantine component library for styling

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import "@mantine/core/styles.css";
+import { MantineProvider } from "@mantine/core";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(
+  <MantineProvider
+    defaultColorScheme="dark"
+    theme={{
+      breakpoints: {
+        xs: "30em",
+        sm: "48em",
+        md: "64em",
+        lg: "74em",
+        xl: "90em",
+      },
+    }}
+    withGlobalStyles
+    withNormalizeCSS
+  >
+    <App />
+  </MantineProvider>
+);
